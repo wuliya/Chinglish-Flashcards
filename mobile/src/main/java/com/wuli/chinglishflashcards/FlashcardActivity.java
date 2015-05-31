d8e49439a028dc665687ff4a32e8f3dafc3f5644
Liya Wu <liyajwu@gmail.com>
2015-05-31 18:26:11 +0100

    Linked layout to vocab list Added logic to flashcard activity On click button colour changes

--- mobile/src/main/java/com/wuli/chinglishflashcards/FlashcardActivity.java
+++ mobile/src/main/java/com/wuli/chinglishflashcards/FlashcardActivity.java
@@ -1,17 +1,93 @@
 package com.wuli.chinglishflashcards;
 
+import android.graphics.Color;
+import android.graphics.PorterDuff;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+import java.util.Random;
 
 
 public class FlashcardActivity extends ActionBarActivity {
+
+    private static final List<VocabItem> VOCAB_ITEMS = Arrays.asList(new VocabItem[] {
+            new VocabItem("中国", "zhōng guó", "China"),
+            new VocabItem("澳大利亚", "Ào dà lì yǎ", "Australia"),
+            new VocabItem("英国", "Yīng guó", "England")
+    });
+    public static final int NUMBER_OF_ANSWERS = 3;
+
+    private List<TextView> answerButtons = new ArrayList<>();
+
+    private int index = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_flashcard);
+
+        answerButtons.add((TextView) findViewById(R.id.answer1));
+        answerButtons.add((TextView) findViewById(R.id.answer2));
+        answerButtons.add((TextView) findViewById(R.id.answer3));
+
+        Collections.shuffle(VOCAB_ITEMS);
+        fillInCardAndAnswers();
+    }
+
+    private void fillInCardAndAnswers() {
+        final VocabItem vocabItem = VOCAB_ITEMS.get(index % VOCAB_ITEMS.size());
+        ((TextView) findViewById(R.id.flashcard_language)).setText(vocabItem.chineseChar);
+        ((TextView) findViewById(R.id.flashcard_pinyin)).setText(vocabItem.chinesePinyin);
+        final List<String> answers=generateAnswers(vocabItem);
+        Collections.shuffle(answers);
+        for (int i = 0; i < NUMBER_OF_ANSWERS; i++) {
+            final String answer = answers.get(i);
+            final TextView button = answerButtons.get(i);
+            button.setText(answer);
+            button.getBackground().clearColorFilter();
+            button.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    if (answer == vocabItem.english) {
+                        button.getBackground().setColorFilter(Color.GREEN, PorterDuff.Mode.MULTIPLY);
+                        v.postDelayed(new Runnable() {
+                            @Override
+                            public void run() {
+                                fillInCardAndAnswers();
+                            }
+                        }, 2000);
+                    } else {
+                        button.getBackground().setColorFilter(Color.RED, PorterDuff.Mode.MULTIPLY);
+                    }
+                }
+            });
+        }
+        index++;
+    }
+
+    private List<String> generateAnswers(VocabItem vocabItem) {
+        List<String> answers = new ArrayList<>();
+        answers.add(vocabItem.english);
+
+        Random random = new Random();
+        while(answers.size() < NUMBER_OF_ANSWERS) {
+            int randomindex = random.nextInt(VOCAB_ITEMS.size());
+            String altAnswer = VOCAB_ITEMS.get(randomindex).english;
+            if (!answers.contains(altAnswer)) {
+                answers.add(altAnswer);
+            }
+        }
+
+        return answers;
     }
 
     @Override
---
+++ mobile/src/main/java/com/wuli/chinglishflashcards/VocabItem.java
@@ -0,0 +1,16 @@
+package com.wuli.chinglishflashcards;
+
+/**
+ * Created by liyawu on 31/05/2015.
+ */
+public class VocabItem {
+    String chineseChar;
+    String chinesePinyin;
+    String english;
+
+    public VocabItem(String chineseChar, String chinesePinyin, String english) {
+        this.chineseChar = chineseChar;
+        this.chinesePinyin = chinesePinyin;
+        this.english = english;
+    }
+}
